c96e151b41342658ed98cf5eba153c1c8b5ce550
dennisdoublea18 <dennisarias1996@yahoo.com>
2019-10-16 14:34:58 -0400

    Changed firstName and lastNam

--- hello.java
+++ hello.java
@@ -5,8 +5,8 @@ public class hello {
     public static void main(String [] args) {
 
 
-        String firstName = "Elizabeth";
-        String lastName = "Jones";
+        String firstName = "Dennis";
+        String lastName = "Arias";
 	
         System.out.println("Hello Class!");
 
